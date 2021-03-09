e7c599cccf2e6bd0ad58a5c51b4d1d8b7f2b045e
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-03-09 10:13:40 +0100

    completo
    aiuto con la sintassi in classe, dettagli e design da completare

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".drop_menu").mouseenter(function() {
+        $(".cont_menu").css("display","block")     
+    })
+    $(".drop_menu").mouseleave(function() {
+        $(".cont_menu").css("display","none")     
+    })
+
+})
