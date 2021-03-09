9c104cce44d9928420c8d72a86a2f78124bfa659
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-03-09 10:57:59 +0100

    brutto ma completo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-    $(".drop_menu").mouseenter(function() {
-        $(".cont_menu").css("display","block")     
-    })
-    $(".drop_menu").mouseleave(function() {
-        $(".cont_menu").css("display","none")     
-    })
+    dropMenu(".tarif_menu", ".drop_tarif_menu");
+    dropMenu(".conti_menu", ".drop_conti_menu");
+    dropMenu(".prodotti_menu", ".drop_prodotti_menu");
+    dropMenu(".azienda_menu", ".drop_azienda_menu");
+    dropMenu(".aiuto_menu", ".drop_aiuto_menu");
+    
 
 })
+
+function dropMenu(pM, dM){
+    $(pM).mouseenter(function() {
+        $(dM).css("display","block")     
+    })
+    $(pM).mouseleave(function() {
+        $(dM).css("display","none")     
+    })
+}
